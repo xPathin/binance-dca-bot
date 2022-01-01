ca4b62132f92072d4f8212fec7e2f4f1a6806496
Patrick Fischer <pfischer@palow.org>
2022-01-01 13:34:36 +0100

    added compose file

--- trades.js
+++ trades.js
@@ -1,9 +1,4 @@
 const trades = [
-	{
-		asset: "ETH", // Asset you want to buy
-		currency: "BTC", // Currency you want to buy the asset with
-		quoteOrderQty: 0.1, // Buy 0.1 BTC worth of ETH
-	}
 ];
 
 export { trades };
